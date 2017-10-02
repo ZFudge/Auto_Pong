40e569ba1b590b526af755afb0f2300c8a6873c4
zfudge <zacheryfudge@gmail.com>
2017-10-02 15:26:04 -0600

    fixed linetracking error

--- auto_pong.js
+++ auto_pong.js
@@ -17,102 +17,104 @@ game.context = game.canvas.getContext('2d');
 
 let diff = document.getElementById('diff');
 
-//ball object
-ping = {
+ball = {
 	x: game.canvas.width/2-5,
 	y: game.canvas.height/2-5,
 	vertical: 0,
 	horizontal: 0,
 	size: 10,
 	line: [],
-	// counts each position that the ball cross and stores them in the ping.line array
-	lineage: false,
+	// determines if the path the ball will follow will be stored in the ball.line array
+	trackline: false,
+	// randomizes the direction of a freshly served ball
 	serve: function() {
 		let rand1 = Math.random();
 		let rand2 = Math.random();
-		ping.vertical = game.playerSpeed * 1.5;
-		ping.horizontal = game.playerSpeed * 1.5;
+		ball.vertical = game.playerSpeed * 1.5;
+		ball.horizontal = game.playerSpeed * 1.5;
 
 		if (rand1 < 0.5) {
-			ping.vertical += -ping.vertical*2;
+			ball.vertical += -ball.vertical*2;
 		}
 		if (rand2 < 0.5) {
-			ping.horizontal += -ping.horizontal*2;
+			ball.horizontal += -ball.horizontal*2;
 		}
 
-		ping.x = game.canvas.width/2-5;
-		ping.y = game.canvas.height/2-5;
+		ball.x = game.canvas.width/2-5;
+		ball.y = game.canvas.height/2-5;
 
 		player.xInt = false;
 		playerTwo.xInt = false;
 	},
+	// bounces the ball if it hits a well, then applies horizontal and vertical speeds to the ball
 	adjust: function() {
-		if (ping.x < 0) {
-			//console.log('bounce','ping.x: ' + ping.x);
-			ping.horizontal += (ping.horizontal*-2);
-			ping.x = Math.abs(ping.x);
-			//console.log('ping.x: ' + ping.x);
-		} else if (ping.x + ping.size > game.canvas.width) {
-			ping.horizontal -= (ping.horizontal*2);
-			ping.x = game.canvas.width-ping.size-(ping.x-(game.canvas.width-ping.size));
+		if (ball.x < 0) {
+			//console.log('bounce','ball.x: ' + ball.x);
+			ball.horizontal += (ball.horizontal*-2);
+			ball.x = Math.abs(ball.x);
+			//console.log('ball.x: ' + ball.x);
+		} else if (ball.x + ball.size > game.canvas.width) {
+			ball.horizontal -= (ball.horizontal*2);
+			ball.x = game.canvas.width-ball.size-(ball.x-(game.canvas.width-ball.size));
 		}
 
-		ping.x += ping.horizontal;	
-		ping.y += ping.vertical;
+		ball.x += ball.horizontal;	
+		ball.y += ball.vertical;
 	},
+	// checks to see if the ball has hit a player
 	collisionCheck: function() {
-		//if (ping.x >= player.x && ping.x <= player.x + player.width && ping.y + ping.size >= player.y && ping.y <= player.y + player.height) {
-		if ( (ping.x >= player.x && ping.x <= player.x + player.width && ping.y + ping.size >= player.y && ping.y + ping.size <= player.y + player.height) || 
-			(ping.x + ping.size >= player.x && ping.x + ping.size <= player.x + player.width && ping.y + ping.size >= player.y && ping.y + ping.size <= player.y + player.height)) {
-			ping.vertical -= (ping.vertical*2);
+		//if (ball.x >= player.x && ball.x <= player.x + player.width && ball.y + ball.size >= player.y && ball.y <= player.y + player.height) {
+		if ( (ball.x >= player.x && ball.x <= player.x + player.width && ball.y + ball.size >= player.y && ball.y + ball.size <= player.y + player.height) || 
+			(ball.x + ball.size >= player.x && ball.x + ball.size <= player.x + player.width && ball.y + ball.size >= player.y && ball.y + ball.size <= player.y + player.height)) {
+			ball.vertical -= (ball.vertical*2);
 
 			/*
-			if (ping.y < playerTwo.y + playerTwo.height) {
-				ping.y = ping.y + ((playerTwo.y + playerTwo.height - ping.y) * 2);
-			} else if (ping.y + ping.size > player.y) {
-				ping.y = ping.y - (( (ping.y + ping.size) - player.y) * 2);
+			if (ball.y < playerTwo.y + playerTwo.height) {
+				ball.y = ball.y + ((playerTwo.y + playerTwo.height - ball.y) * 2);
+			} else if (ball.y + ball.size > player.y) {
+				ball.y = ball.y - (( (ball.y + ball.size) - player.y) * 2);
 			}
 			*/
 
-			ping.y = ping.y - (( (ping.y + ping.size) - player.y) * 2);
+			ball.y = ball.y - (( (ball.y + ball.size) - player.y) * 2);
 
-			let pingRange = (ping.x + ping.size/2 - player.x - 20) / 10;
-			if (Math.abs(ping.horizontal < 15)) {
-				ping.horizontal += pingRange;
+			let ballRange = (ball.x + ball.size/2 - player.x - 20) / 10;
+			if (Math.abs(ball.horizontal < 15)) {
+				ball.horizontal += ballRange;
 			}
-			ping.horizontal.toFixed(4);
+			ball.horizontal.toFixed(4);
 
-			//diff.innerHTML = Math.abs((ping.x + ping.size/2) - player.xInt);
+			//diff.innerHTML = Math.abs((ball.x + ball.size/2) - player.xInt);
 
 			player.xInt = false;
 			game.bounces++;
 			game.score.innerHTML = game.bounces;
-		} else if ((ping.x >= playerTwo.x && ping.x <= playerTwo.x + playerTwo.width && ping.y >= playerTwo.y && ping.y <= playerTwo.y + playerTwo.height) ||
-			(ping.x + ping.size >= playerTwo.x && ping.x + ping.size <= playerTwo.x + playerTwo.width && ping.y >= playerTwo.y && ping.y <= playerTwo.y + playerTwo.height)) {
-			ping.vertical += (ping.vertical*-2);
+		} else if ((ball.x >= playerTwo.x && ball.x <= playerTwo.x + playerTwo.width && ball.y >= playerTwo.y && ball.y <= playerTwo.y + playerTwo.height) ||
+			(ball.x + ball.size >= playerTwo.x && ball.x + ball.size <= playerTwo.x + playerTwo.width && ball.y >= playerTwo.y && ball.y <= playerTwo.y + playerTwo.height)) {
+			ball.vertical += (ball.vertical*-2);
 
-			ping.y = ping.y + ((playerTwo.y + playerTwo.height - ping.y) * 2);
+			ball.y = ball.y + ((playerTwo.y + playerTwo.height - ball.y) * 2);
 
-			let pingRange = (ping.x + ping.size/2 - playerTwo.x - 20) / 10;
-			ping.horizontal += pingRange;
-			ping.horizontal.toFixed(4);
+			let ballRange = (ball.x + ball.size/2 - playerTwo.x - 20) / 10;
+			ball.horizontal += ballRange;
+			ball.horizontal.toFixed(4);
 
-			//diff.innerHTML = Math.abs((ping.x + ping.size/2) - playerTwo.xInt)
+			//diff.innerHTML = Math.abs((ball.x + ball.size/2) - playerTwo.xInt)
 
 			playerTwo.xInt = false;
 			game.bounces++;
 			game.score.innerHTML = game.bounces;
 		}
-		// ping.vertical += -ping.vertical * 2;
+		// ball.vertical += -ball.vertical * 2;
 	},
 	checkScore: function() {
-		if (ping.y <= 0) {
-			ping.serve();
+		if (ball.y <= 0) {
+			ball.serve();
 			document.body.style.backgroundColor = 'blue';
 			//game.active = !game.active;
 			clearInterval(pongInterval)
-		} else if (ping.y + ping.size >= game.canvas.height) {
-			ping.serve();
+		} else if (ball.y + ball.size >= game.canvas.height) {
+			ball.serve();
 			document.body.style.backgroundColor = 'blue';
 			//game.active = !game.active;
 			clearInterval(pongInterval)
@@ -136,33 +138,33 @@ class Player {
 			if (this.x + this.width >= game.canvas.width && this.horizontal > 0 || this.x <= 0 && this.horizontal < 0) this.horizontal = 0;
 		}
 		// continuously checks the position of the ball's x axis and adjusts the player accordingly
-		this.checkPingConst = function() {
-			if (ping.x < this.x && (ping.vertical < 0 && this.y < game.canvas.height/2 || ping.vertical > 0 && this.y > game.canvas.height/2) ) {
+		this.checkBallConst = function() {
+			if (ball.x < this.x && (ball.vertical < 0 && this.y < game.canvas.height/2 || ball.vertical > 0 && this.y > game.canvas.height/2) ) {
 				this.horizontal = -game.playerSpeed;
-			} else if (ping.x > this.x + this.width && (ping.vertical < 0 && this.y < game.canvas.height/2 || ping.vertical > 0 && this.y > game.canvas.height/2) ) {
+			} else if (ball.x > this.x + this.width && (ball.vertical < 0 && this.y < game.canvas.height/2 || ball.vertical > 0 && this.y > game.canvas.height/2) ) {
 				this.horizontal = game.playerSpeed;
-			} else if (ping.x > this.x + this.width/4 && ping.x < this.x + this.width * 0.75) {
+			} else if (ball.x > this.x + this.width/4 && ball.x < this.x + this.width * 0.75) {
 				this.horizontal = 0;
 			}
 		}
 		// checks player's xInt location in comparison to which side it intends to bounce the ball with and adjusts player accordingly
-		this.checkPing = function() {
-			if (this.xInt < 0 || this.xInt > game.canvas.width || !this.xInt && (ping.vertical > 0 && ping.y < this.y || ping.vertical < 0 && ping.y > this.y) ) {
+		this.checkBall = function() {
+			if (this.xInt < 0 || this.xInt > game.canvas.width || !this.xInt && (ball.vertical > 0 && ball.y < this.y || ball.vertical < 0 && ball.y > this.y) ) {
 				//console.log('xInt is false');
 				this.dest();
 			} else { //
 				if (this.side) { // left
-					if (this.x + ping.size/1.5 > this.xInt) {
+					if (this.x + ball.size/1.5 > this.xInt) {
 						this.horizontal = -game.playerSpeed;
-					} else if (this.x + ping.size*1.5 < this.xInt && (ping.vertical > 0 && ping.y < this.y || ping.vertical < 0 && ping.y > this.y)) {
+					} else if (this.x + ball.size*1.5 < this.xInt && (ball.vertical > 0 && ball.y < this.y || ball.vertical < 0 && ball.y > this.y)) {
 						this.horizontal = game.playerSpeed;
 					} else {
 						this.horizontal = 0;
 					}
 				} else { // right
-					if (this.x + this.width - ping.size/1.5 < this.xInt) {
+					if (this.x + this.width - ball.size/1.5 < this.xInt) {
 						this.horizontal = game.playerSpeed;
-					} else if (this.x + this.width - ping.size*1.5 > this.xInt && (ping.vertical > 0 && ping.y < this.y || ping.vertical < 0 && ping.y > this.y)) {
+					} else if (this.x + this.width - ball.size*1.5 > this.xInt && (ball.vertical > 0 && ball.y < this.y || ball.vertical < 0 && ball.y > this.y)) {
 						this.horizontal = -game.playerSpeed;
 					} else {
 						this.horizontal = 0;
@@ -171,7 +173,7 @@ class Player {
 				/* keeps ball centered
 				if (this.x + 22.5 < this.xInt) {
 					this.horizontal = game.playerSpeed;
-				} else if (this.x + 17.5 > this.xInt && (ping.vertical > 0 && ping.y < this.y || ping.vertical < 0 && ping.y > this.y)) {
+				} else if (this.x + 17.5 > this.xInt && (ball.vertical > 0 && ball.y < this.y || ball.vertical < 0 && ball.y > this.y)) {
 					this.horizontal = -game.playerSpeed;
 				} else {
 					this.horizontal = 0;
@@ -183,20 +185,20 @@ class Player {
 		// to calculate where the ball will intersect with the goal
 		this.dest = function() {
 			let w = game.canvas.width;
-			let h = ping.horizontal; 
-			let v = Math.abs(ping.vertical);
-			let yD = Math.abs(ping.y - this.y);
-			yD -= ping.size/2;
+			let h = ball.horizontal; 
+			let v = Math.abs(ball.vertical);
+			let yD = Math.abs(ball.y - this.y);
+			yD -= ball.size/2;
 			// 
 			this.side = Math.random() > 0.5;
 
 			let y = Math.round(yD/v);
-			let x = ping.x;
+			let x = ball.x;
 			
-			if (ping.lineage) {
-				ping.line = [];
-				let a = 0;
-				let b = ping.y < game.canvas.height/2; // upper side
+			if (ball.trackline) {
+				ball.line = [];
+				var a = 0;
+				let b = ball.y < game.canvas.height/2; // upper side
 
 				if (b) {
 					a = playerTwo.y + playerTwo.height;
@@ -206,26 +208,26 @@ class Player {
 			}
 
 			for(let i=0;i<y;i++) {
-				if (ping.lineage) {   
-					a += ping.vertical;
-					ping.line.push(
+				if (ball.trackline) {   
+					a += ball.vertical;
+					ball.line.push(
 						{
-							xa: x + ping.size/2,
+							xa: x + ball.size/2,
 							ya: a
 						});
 				}
 
 				x += h;
-				if (x + ping.size > w || x < 0) {
+				if (x + ball.size > w || x < 0) {
 					h += -h*2;
 					if (x < 0) {
 						x += -x*2;
 					} else {
-						x = w-ping.size - (x - (w-ping.size) );
+						x = w-ball.size - (x - (w-ball.size) );
 					}
 				}
 			}
-			this.xInt = Math.round(x + ping.size/2);
+			this.xInt = Math.round(x + ball.size/2);
 		}
 
 	}
@@ -233,16 +235,16 @@ class Player {
 
 // draws the path the ball will follow
 function linedraw() {
-	for(let prop in ping.line) {
+	for(let prop in ball.line) {
 		game.context.fillStyle = '#0FF';
-		game.context.fillRect(ping.line[prop].xa,ping.line[prop].ya,1,1);
+		game.context.fillRect(ball.line[prop].xa,ball.line[prop].ya,1,1);
 	}
 }
 
 let playerTwo = new Player(game.playerTwoPos);
 let player = new Player(game.playerOnePos);
 
-ping.serve();
+ball.serve();
 pongInterval = setInterval(pong,game.speed);
 
 function pong() {
@@ -253,27 +255,27 @@ function pong() {
 	game.context.fillRect(playerTwo.x,playerTwo.y,playerTwo.width,playerTwo.height);
 	game.context.fillRect(player.x,player.y,player.width,player.height);
 
-	game.context.fillRect(ping.x,ping.y,ping.size,ping.size);
+	game.context.fillRect(ball.x,ball.y,ball.size,ball.size);
 
-	if (ping.lineage) {
+	if (ball.trackline) {
 		linedraw();
 	}
 	game.context.fillStyle = '#0F0';
-	game.context.fillRect(ping.x+4,ping.y+4,2,2);
+	game.context.fillRect(ball.x+4,ball.y+4,2,2);
 
-	player.checkPing();
+	player.checkBall();
 	player.checkSide();
 
 	player.x += player.horizontal;
 
-	playerTwo.checkPing();
+	playerTwo.checkBall();
 	playerTwo.checkSide();
 
 	playerTwo.x += playerTwo.horizontal;
 
-	ping.adjust();
-	ping.collisionCheck();
-	ping.checkScore();
+	ball.adjust();
+	ball.collisionCheck();
+	ball.checkScore();
 
 	//console.log('pong');
 }
@@ -290,7 +292,7 @@ function pushedKey(btn) {
 	} else if (btn.keyCode === 39) {
 		player.horizontal = 1;
 	} else if (btn.keyCode === 76) {
-		ping.lineage = !ping.lineage;
+		ball.trackline = !ball.trackline;
 	}
 
 	if (btn.keyCode === 32) {
@@ -315,11 +317,11 @@ function stopAndGo() {
 }
 
 function refresh() {
-	ping.serve();
+	ball.serve();
 	document.body.style.backgroundColor = 'white';
 }
 
 
 game.canvas.onclick = function() {
-	ping.lineage = !ping.lineage;
+	ball.trackline = !ball.trackline;
 }
